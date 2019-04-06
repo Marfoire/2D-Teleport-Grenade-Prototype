8c0167d098d3906cf46b0a47ed4293286e99eaac
Lori <48799995+lrsgbh@users.noreply.github.com>
2019-04-06 17:03:27 -0400

    added collectalble thing, no UI yet but the variable will count it in script

---
+++ Assets/Scripts/Player/PlayerCollectables.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollectables : MonoBehaviour
+{
+    public int totalCollected;
+
+    public void IncrementCollected() {
+        totalCollected++;
+    }
+}
