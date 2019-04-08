3307d0bb2642aa464de08f3ed4de006032bc23c6
Marko <mellowofc.business@gmail.com>
2019-04-08 02:58:02 -0400

    Added level end / restart.
    Added a box collider to the flag as well as the restart script. We can change the scene to some sort of UI if we want to.
    
    This would have been implemented MUCH sooner however, I've been working on the shooter enemy which has been giving me issues.....

---
+++ Assets/Scripts/Environment/Restart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    private Scene scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene(); 
+    }
+
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(scene.name);
+        }
+        else 
+        {
+
+
+        }
+    }
+}
