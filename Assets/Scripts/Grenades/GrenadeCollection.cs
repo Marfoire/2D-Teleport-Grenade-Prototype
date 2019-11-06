55beed78c066349530e710ce49829db3a36feac9
Jackson Lanaus <jackson.lanaus@gmail.com>
2019-11-06 10:55:42 -0500

    adding greand pickups

---
+++ Assets/Scripts/Grenades/GrenadeCollection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeCollection : MonoBehaviour
+{
+
+    public PlayerController playerCon;
+
+    public GameObject[] GrenadeArray1;
+    public GameObject[] GrenadeArray2;
+    public GameObject[] GrenadeArray3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCon = GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerCon.grenadeTypePrefabs = GrenadeArray1;
+    }
+}
