203d53edfd8d351983b2ed6ee858ad0d843282ce
Jackson Lanaus <38150077+380Jackson@users.noreply.github.com>
2019-12-06 15:05:00 -0500

    Doors work / Line shader made

--- Assets/Scripts/Environment/DoorButtonController.cs
+++ Assets/Scripts/Environment/DoorButtonController.cs
@@ -8,22 +8,43 @@ public class DoorButtonController : MonoBehaviour
     public bool DoorIsOn;
     public MotionDetection MD;
     public Animator anim;
+    public GameObject DoorCol;
     
     // Start is called before the first frame update
     void Start()
     {
         DoorIsOn = false;
+        DoorCol.SetActive(true);
         anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
 
+        
         if (DoorUseMotion == true)
         {
-            anim.SetBool("DoorIsOn", MD.Open);
+            if (MD.Open == true)
+            {
+                anim.SetBool("DoorIsOn", MD.Open);
+                DoorIsOn = true;
+            }
+            if (MD.Open == false)
+            {
+                anim.SetBool("DoorIsOn", MD.Open);
+                DoorIsOn = false;
+            }
+
+        }
+
+        if (DoorIsOn == false)
+        {
+            DoorCol.SetActive(true);
+        }
+        if (DoorIsOn == true)
+        {
+            DoorCol.SetActive(false);
         }
     }
 
